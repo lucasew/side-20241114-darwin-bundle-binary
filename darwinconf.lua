f09cbebcefc5ee6343597bb96ccc486eb0f110d3
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-11-14 13:43:54 -0300

    first

---
+++ darwinconf.lua
@@ -0,0 +1,11 @@
+darwin.embed_global("help", dtw.load_file("help.txt"))
+local concat_path = true
+local src_files = dtw.list_files_recursively("src", concat_path)
+for i = 1, #src_files do
+    local current = src_files[i]
+    darwin.add_lua_code("-- file: " .. current .. "\n")
+    darwin.add_lua_file(current)
+end
+darwin.add_lua_code("main()") -- these its required to start main
+darwin.generate_c_executable_output({ output_name = "bundle.c" })
+os.execute("gcc bundle.c -o bundle.o")
---
+++ install_dependencies.sh
@@ -0,0 +1,1 @@
+curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.013/darwin013.c -o darwin013.c
